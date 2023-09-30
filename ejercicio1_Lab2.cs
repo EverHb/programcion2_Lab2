9e9acdf88757d3eb118701304ee99fb839d49618
EverHb <everbenavides1234@gmail.com>
2023-09-30 16:00:00 -0600

    laboratorio 2

---
+++ ejercicio1_Lab2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class EjercicioLab2
+{
+    static void Main(string[]arg)
+    {
+
+        Console.Write("Ingrese la coordenada x del punto 1: ");
+        double x1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la coordenada y del punto 1: ");
+        double y1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la coordenada x del punto 2: ");
+        double x2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la coordenada y del punto 2: ");
+        double y2 = double.Parse(Console.ReadLine());
+
+        double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        Console.WriteLine($"La distancia entre los puntos ({x1}, {y1}) y ({x2}, {y2}) es: {distancia}");
+    }
+}
---
+++ ejercicio2_Lab2.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+
+class Ejercicio2Lab2
+{
+    static void Main()
+    {
+        DateTime fechaActual = DateTime.Now;
+
+        int diaActual = fechaActual.Day;
+        int mesActual = fechaActual.Month;
+        int añoActual = fechaActual.Year;
+
+        DateTime navidad = new DateTime(añoActual, 12, 25);
+        DateTime añoNuevo = new DateTime(añoActual + 1, 1, 1);
+        TimeSpan tiempoHastaNavidad = navidad - fechaActual;
+        TimeSpan tiempoHastaAñoNuevo = añoNuevo - fechaActual;
+
+        int diasParaNavidad = tiempoHastaNavidad.Days;
+        int diasParaAñoNuevo = tiempoHastaAñoNuevo.Days;
+
+        Console.WriteLine("Día actual: " + diaActual);
+        Console.WriteLine("Mes actual: " + mesActual);
+        Console.WriteLine("Año actual: " + añoActual);
+        Console.WriteLine("Días faltan para Navidad: " + diasParaNavidad);
+        Console.WriteLine("Días faltan para Año Nuevo: " + diasParaAñoNuevo);
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
